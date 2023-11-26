0eeed4730d96314f2ef472a79e18add97d40157a
EdiedRamos <edieisa22@gmail.com>
2023-11-26 01:10:09 -0500

    add geo service

--- src/components/SearchButton/SearchButton.tsx
+++ src/components/SearchButton/SearchButton.tsx
@@ -1,9 +1,16 @@
 import { Button } from "..";
 import { ArrowIcon } from "../../assets/images";
+import { Geo } from "../../services/api";
 
 export const SearchButton = () => {
   return (
-    <Button>
+    <Button
+      onClick={() => {
+        Geo.getInfoFromIp("")
+          .then((res) => console.log({ res }))
+          .catch((error) => console.log({ error }));
+      }}
+    >
       <img src={ArrowIcon} width="10px" />
     </Button>
   );
---
+++ src/services/api/index.ts
@@ -0,0 +1,14 @@
+// It could be save in a .env file but It's gonna be visible in the request so it doestn't matter.
+// By the way, the api service is free.
+const apiBase =
+  "https://geo.ipify.org/api/v2/country?apiKey=at_QD2nQUye6tMFENOVYv7GtX3A0FQOB&ipAddress=IP_DIR";
+
+export const Geo = {
+  getInfoFromIp: (ip: string) =>
+    new Promise((resolve, reject) => {
+      fetch(apiBase.replace("IP_DIR", ip))
+        .then((response) => response.json())
+        .then((data) => resolve(data))
+        .catch((error) => reject(error));
+    }),
+};
