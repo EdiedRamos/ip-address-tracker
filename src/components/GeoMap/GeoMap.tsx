6240aa5614442a76022d474894aa16579e8d07c8
EdiedRamos <edieisa22@gmail.com>
2023-11-26 23:54:02 -0500

    update GeoMap icon and add a tooltip

--- src/components/GeoMap/GeoMap.tsx
+++ src/components/GeoMap/GeoMap.tsx
@@ -1,9 +1,17 @@
 import { useContext, useCallback } from "react";
-import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { Icon } from "leaflet";
 
 import { TrackerContext } from "../../core/context/trackerContext";
 
+import { MarkerIcon } from "../../assets/images";
+
 import "leaflet/dist/leaflet.css";
+
+const customIcon = new Icon({
+  iconUrl: MarkerIcon,
+  iconSize: [35, 35],
+});
 
 export const GeoMap = () => {
   const context = useContext(TrackerContext);
@@ -19,7 +27,9 @@ export const GeoMap = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={position} />
+        <Marker position={position} icon={customIcon}>
+          <Popup>Your IP location</Popup>
+        </Marker>
       </MapContainer>
     );
   }, [context?.tracker]);
